b05c545f88a6ee908f9d66996bf29548f5224e34
luke <luke.lukejoe@gmail.com>
2026-09-01 18:08:47 -0600

    rooms unit test

--- tests/unit/rooms.test.ts
+++ tests/unit/rooms.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, it } from "vitest";
 
-import { createRoom, rooms } from "../../src/rooms.js";
+import { addPeer, createRoom, removePeer, rooms } from "../../src/rooms.js";
 
 afterEach(() => {
   rooms.clear();
@@ -13,5 +13,48 @@ describe("createRoom", () => {
     expect(room.hostId).toBe(67);
     expect(room.peerIds).toEqual([67]);
     expect(room.joinCode).toMatch(/^[A-Z0-9]{6}$/);
+  });
+
+  it("adds the room to the rooms map", () => {
+    const room = createRoom({ hostId: 67 });
+
+    expect(rooms.get(room.joinCode)).toBe(room);
+  });
+});
+
+describe("addPeer", () => {
+  it("adds a peer to to an existing room", () => {
+    const room = createRoom({ hostId: 67 });
+
+    const result = addPeer({ room, peerId: 2 });
+
+    expect(result).toBe(room);
+    expect(room.peerIds).toEqual([67, 2]);
+  });
+});
+
+describe("removePeer", () => {
+  it("removes a peer from an existing room", () => {
+    const room = createRoom({ hostId: 67 });
+
+    addPeer({ room, peerId: 2 });
+    addPeer({ room, peerId: 3 });
+
+    removePeer({
+      room,
+      peerId: 2,
+    });
+
+    expect(room.peerIds).toEqual([67, 3]);
+  });
+});
+
+describe("getRoom", () => {
+  it("returns a room by its join code", () => {
+    const room = createRoom({ hostId: 67 });
+
+    const result = rooms.get(room.joinCode);
+
+    expect(result).toBe(room);
   });
 });
